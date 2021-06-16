64a17d4b06da6578fdd8c5ccfa10c01d2ccaa80e
Cecilio Cannavacciuolo <c.cannavacciuolo@anoki.it>
2021-06-16 22:50:45 +0200

    OK

--- src/runJestTest.js
+++ src/runJestTest.js
@@ -12,7 +12,6 @@ module.exports.runJestTest = async function (options) {
   let testResult = await jest.runCLI(optionsJest, optionsJest.projects);
 
   if (testResult.results.numFailedTestSuites > 0) {
-    process.exit(0);
     throw new Error(chalk.red.bold(` 🛑 SMOKE TEST ERROR 👎`));
   }
 
