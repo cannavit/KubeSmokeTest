c4fb037a0872eb2b408d0adbb15b435d91245745
Cecilio Cannavacciuolo <c.cannavacciuolo@anoki.it>
2021-06-17 17:54:44 +0200

    Test break pipeline

--- src/breakPipeline.js
+++ src/breakPipeline.js
@@ -1,21 +1,22 @@
 const fs = require('fs');
-const chalk = require('chalk');
 
 async function checkPassTest() {
   var passTest = fs.readFileSync('passText.txt', 'utf8');
 
-  console.log('SMKTEST_PASS_TEST:', passTest);
   if (passTest === 'false') {
+    console.log();
     console.log('💨 💨 💨 🔥 💨 💨 💨 🔥 💨 💨 💨 🔥 💨 💨 💨 🔥');
-    console.log(chalk.red.bold(' 🛑  ERROR: Smoke Test'));
+    console.log(' 🛑  ERROR: Smoke Test');
     console.log('💨 💨 💨 🔥 💨 💨 💨 🔥 💨 💨 💨 🔥 💨 💨 💨 🔥');
+    console.log();
+    console.log();
 
     throw new Error('Failed the smoke test');
   } else {
-    console.log(chalk.green.bold(' 🟢 🚭 SUCCESS SMOKE TEST'));
-    console.log(
-      chalk.bold.bold(' ✅ You can continue with the rest of the test suites')
-    );
+    console.log();
+    console.log(' 🟢 🚭 SUCCESS SMOKE TEST');
+    console.log(' ✅ You can continue with the rest of the test suites');
+    console.log();
   }
 }
 
