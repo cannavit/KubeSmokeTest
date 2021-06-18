2c87f239f054bb26cb16163be6dc1a95dac69123
Cecilio Cannavacciuolo <c.cannavacciuolo@anoki.it>
2021-06-18 09:18:30 +0200

    test

--- src/runJestTest.js
+++ src/runJestTest.js
@@ -15,27 +15,28 @@ module.exports.runJestTest = async function (options) {
   let testResult = await jest.runCLI(optionsJest, optionsJest.projects);
 
   if (testResult.results.numFailedTestSuites > 0) {
+    console.log();
+    console.log('-------------------------------------------------------');
     console.log(chalk.red.bold(` 🛑  SMOKE TEST ERROR 👎`));
     console.log(' FORCE BREAK OF THE PIPELINE >>');
-
     console.log();
-    console.log('💨 💨 💨 🔥 💨 💨 💨 🔥 💨 💨 💨 🔥 💨 💨 💨 🔥');
+    console.log('💨 💨 💨 🔥 💨 💨 💨 🔥 💨 💨 💨 🔥 💨 💨 💨 🔥💨 💨 💨 🔥');
     console.log(' 🛑  ERROR: Smoke Test');
-    console.log('💨 💨 💨 🔥 💨 💨 💨 🔥 💨 💨 💨 🔥 💨 💨 💨 🔥');
+    console.log('💨 💨 💨 🔥 💨 💨 💨 🔥 💨 💨 💨 🔥 💨 💨 💨 🔥💨 💨 💨 🔥');
     console.log();
+    console.log('-------------------------------------------------------');
     console.log();
-
-    process.env.SMKTEST_PASS_TEST = false;
 
     process.exit(1); // failed pipeline gitlab
   } else {
     console.log();
-
+    console.log('-------------------------------------------------------');
     console.log();
-    console.log('Job succeeded');
+    console.log(' 🟩 Job succeeded');
     console.log(' 🟢 🚭 SUCCESS SMOKE TEST');
     console.log(' ✅ You can continue with the rest of the test suites');
     console.log();
+    console.log('-------------------------------------------------------');
     console.log();
 
     process.exit(0); //pass pipeline gitlab
