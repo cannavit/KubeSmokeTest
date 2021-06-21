0fd532279613eb2f3ff46ff5ff5eb5bed87c9a55
Cecilio Cannavacciuolo <c.cannavacciuolo@anoki.it>
2021-06-21 14:34:12 +0200

    test

--- src/utils/sendReport.js
+++ src/utils/sendReport.js
@@ -5,18 +5,24 @@ module.exports.sendToSmokeCollector = async function (options) {
   // export async function sendToSmokeCollector(options) {
   // Decode
   console.log(' 💾 SEND REPORT TO SMOKE-COLLECTOR');
+
   let urlSomeCollectorCoded = process.env.SMOKE_COLLECTOR;
 
   let data = options.smokeCollector.data;
 
+  console.log(' 💾 Econde address: ' + urlSomeCollectorCoded);
+
   if (urlSomeCollectorCoded) {
     var urlSomeCollector = base64.decode(urlSomeCollectorCoded) + 'smktest';
+    console.log(' 📬 Deconde Address: ' + urlSomeCollector);
 
+    console.log();
     let result = await axios({
       method: 'post',
       url: urlSomeCollector,
       data: data,
     });
+    console.log();
 
     if (result.status === 200) {
       console.log(' 📝 Send report to smoke-collector');
